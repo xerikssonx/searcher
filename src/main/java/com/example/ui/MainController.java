878228687b11b9160b52e9ea674eb49fdf71ce03
edhar_vakhitov <edhar.vakhitov@cloudtp.com>
2016-08-14 15:52:02 +0300

    add button to table

--- src/main/java/com/example/ui/MainController.java
+++ src/main/java/com/example/ui/MainController.java
@@ -34,7 +34,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javax.imageio.ImageIO;
-
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.util.Callback;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 
 @SuppressWarnings("SpringJavaAutowiringInspection")
 public class MainController {
@@ -45,7 +54,11 @@ public class MainController {
 	@FXML
 	private TextField txtSearch;
 	@FXML
-	private ListView<String> listItems;
+	private TableView<File> table;
+	@FXML
+	private TableColumn columnPath;
+	@FXML
+	private TableColumn columnAction;
 
 	private Logger logger = LoggerFactory.getLogger(MainController.class);
 
@@ -55,16 +68,48 @@ public class MainController {
 	@PostConstruct
 	public void init() {
 		javafx.application.Platform.setImplicitExit(false);
+		columnPath.setCellValueFactory(new PropertyValueFactory<>("path"));
+
+		columnAction.setCellValueFactory(new PropertyValueFactory<>("DUMMY"));
+
+		Callback<TableColumn<File, String>, TableCell<File, String>> cellFactory = new Callback<TableColumn<File, String>, TableCell<File, String>>() {
+			@Override
+			public TableCell call(final TableColumn<File, String> param) {
+				final TableCell<File, String> cell = new TableCell<File, String>() {
+
+					final Button btn = new Button("Copy");
+
+					@Override
+					public void updateItem(String item, boolean empty) {
+						super.updateItem(item, empty);
+						if (empty) {
+							setGraphic(null);
+							setText(null);
+						} else {
+							btn.setOnAction((ActionEvent event) -> {
+								File person = getTableView().getItems().get(getIndex());
+								System.out.println(person.getPath());
+							});
+							setGraphic(btn);
+							setText(null);
+						}
+					}
+				};
+				return cell;
+			}
+		};
+
+		columnAction.setCellFactory(cellFactory);
+
 	}
 
 	@FXML
 	public void search() {
 		List<File> list = searchService.search(txtSearch.getText());
 		List<String> strings = list.stream().map(i -> i.getPath()).collect(Collectors.toList());
-		ObservableList<String> observableList = FXCollections.<String>observableArrayList(strings);
-		listItems.getItems().clear();
-		listItems.getItems().addAll(observableList);
-
+		ObservableList<File> observableList = FXCollections.<File>observableArrayList(list);
+		table.getItems().clear();
+		table.setItems(observableList);
 	}
 
 }
